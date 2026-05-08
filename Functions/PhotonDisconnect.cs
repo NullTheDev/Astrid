787b3d9d23ecb6de4028a6e3ea1e53151f5a011d
NullTheDev <himynameisjohncena9@gmail.com>
2026-05-08 16:16:32 -0400

    I forgot the brackets lol

--- Functions/PhotonDisconnect.cs
+++ Functions/PhotonDisconnect.cs
@@ -2,7 +2,7 @@ using Photon.Realtime;
 using Photon.Pun;
 using System;
 
-public static void PhotonDisconnect()
-  PhotonNetwork.Disconnect()
+public static void PhotonDisconnect();
+  PhotonNetwork.Disconnect();
   return true;
 }
